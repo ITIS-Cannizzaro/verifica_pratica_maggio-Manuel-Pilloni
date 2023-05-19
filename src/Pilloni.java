782fb5f015caa27aefe3f05d21b1336af96fbcd0
camil <camil@LAPTOP-B56TEV94>
2023-05-19 16:15:24 +0200

    Consegna Verifica com correzioni!

--- src/Pilloni.java
+++ src/Pilloni.java
@@ -2,4 +2,5 @@ public class Pilloni{
 	public static void main(String[] args){
 		Gui gui= new Gui();
 	}
-}
+}// Va bene, ma il tasto "rimuovi" non funziona perchè invece di rimuoverli fisicamente dall'elenco, il codice imposta semplicemente il valore dell'elemento corrispondente a null. 
+//E questo può essere un problema se si cerca di visualizzare l'Array dopo aver rimosso un numero, infatti non te lo fà visualizzare!
